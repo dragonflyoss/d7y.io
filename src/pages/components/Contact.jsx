e49ca8fe8ede8d059897c52fa415facc730ab2f9
zhaoxinxin <1186037180@qq.com>
2025-04-15 19:08:14 +0800

    chore: bump lottie-react version to v2.41
    Signed-off-by: zhaoxinxin <1186037180@qq.com>

--- src/pages/components/Contact.jsx
+++ src/pages/components/Contact.jsx
@@ -1,15 +1,13 @@
-import React from "react";
-import Lottie from "lottie-react";
+import React from 'react';
+import Lottie from 'lottie-react';
 import ContactAnimation from '../../../static/lottie/contact.json';
 
 const Style = {
-    height: 58,
-    width:'100%'
-  };
+  height: 58,
+  width: '100%',
+};
 
-const Contact = ()=>{
-    return(
-        <Lottie animationData={ContactAnimation} style={Style}> </Lottie>
-    )
-}
+const Contact = () => {
+  return <Lottie animationData={ContactAnimation} style={Style} />;
+};
 export default Contact;
